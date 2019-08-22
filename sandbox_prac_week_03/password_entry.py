6691aada2bce8863139baec05704e7f2b02c697f
Callum Gedling <callum.gedling@my.jcu.edu.au>
2019-08-22 11:34:51 +1000

    Add password check program

--- sandbox_prac_week_03/password_entry.py
+++ sandbox_prac_week_03/password_entry.py
@@ -1,1 +1,7 @@
 """Callum"""
+MIN_LENGTH = 4
+password = input("Please enter a password:")
+while len(password) < MIN_LENGTH:
+    password = input("Please enter a password with four or more letters:")
+for letters in password:
+    print("*", end="")
